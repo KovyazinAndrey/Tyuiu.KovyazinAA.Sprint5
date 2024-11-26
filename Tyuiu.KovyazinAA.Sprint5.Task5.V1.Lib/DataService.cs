b3c8b8d83a648eb0a591000beffa95e6e840825f
Kovyazin Andrey <hiroazora@gmail.com>
2024-11-26 12:29:08 +0500

    Task5

---
+++ Tyuiu.KovyazinAA.Sprint5.Task5.V1.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KovyazinAA.Sprint5.Task5.V1.Lib
+{
+    public class DataService : ISprint5Task5V1
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] str = line.Split(',');
+                    foreach (string str2 in str)
+                    {
+                        if (Convert.ToDouble(str2) % 2 == 0)
+                        {
+                            string str3 = str2.Replace(".", ",");
+                            res = res + Convert.ToDouble(str2);
+                        }
+                    }
+                }
+            }
+            res = Math.Round(res,3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KovyazinAA.Sprint5.Task5.V1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KovyazinAA.Sprint5.Task5.V1.Lib;
+namespace Tyuiu.KovyazinAA.Sprint5.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Ковязин А. А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Ковязин Андрей Александрович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл в котором есть набор значений, найти сумму всех чисел с        *");
+            Console.WriteLine("* признаками деления на 2                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V1.txt";
+            Console.WriteLine("* Путь: " + path + " *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.LoadFromDataFile(path));
+        }
+    }
+}
