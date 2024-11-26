962101fd15942eff6ed465f5cb02a61a5969021d
Kovyazin Andrey <hiroazora@gmail.com>
2024-11-26 21:49:26 +0500

    Task6

---
+++ Tyuiu.KovyazinAA.Sprint5.Task6V28.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KovyazinAA.Sprint5.Task6V28.Lib
+{
+    public class DataService : ISprint5Task6V28
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                int countnum = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        char c = line[i];
+                        if (char.IsLetter(c))
+                        {
+                            countnum = 0;
+                            continue;
+                        }
+                        else if (char.IsDigit(c))
+                        {
+                            if (countnum < 4) 
+                            {
+                                countnum++;
+                                continue;
+                            }
+                            else if (countnum == 4)
+                            {
+                                count++;
+                                countnum = 0;
+                                continue;
+                            }
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ Tyuiu.KovyazinAA.Sprint5.Task6V28/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KovyazinAA.Sprint5.Task6V28.Lib;
+namespace Tyuiu.KovyazinAA.Sprint5.Task6V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #6 | Выполнил: Ковязин А. А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #6                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Ковязин Андрей Александрович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл в котором есть набор символьных данных. Найти количество       *");
+            Console.WriteLine("* четырехзначных чисел в заданной строке.                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask6V28.txt";
+            Console.WriteLine("* Путь к файлу: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.LoadFromDataFile(path));
+        }
+    }
+}
