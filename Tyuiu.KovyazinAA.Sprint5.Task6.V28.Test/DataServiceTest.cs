99cf0fd4531735e7b976b3301d3b1a7591c88af9
Kovyazin Andrey <hiroazora@gmail.com>
2024-11-26 21:58:45 +0500

    Task6

---
+++ Tyuiu.KovyazinAA.Sprint5.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using Tyuiu.KovyazinAA.Sprint5.Task6.V28.Lib;
+namespace Tyuiu.KovyazinAA.Sprint5.Task6.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V28.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool file = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, file);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V28.txt";
+            var res = ds.LoadFromDataFile(path);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+}
